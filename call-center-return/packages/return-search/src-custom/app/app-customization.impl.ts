239237f03ff7914da3e0bd6128a99fb8be08f89f
Madhavi <9113237@ulta.com>
2026-03-20 16:53:31 +0530

    test

--- call-center-return/packages/return-search/src-custom/app/app-customization.impl.ts
+++ call-center-return/packages/return-search/src-custom/app/app-customization.impl.ts
@@ -1,7 +1,7 @@
 
 
 export class AppCustomizationImpl {
-    static readonly components = [];
-    static readonly providers = [ReturnSearchComponent1];
+    static readonly components = [SampleSharedComponent];
+    static readonly providers = [];
     static readonly imports = [];
 }
