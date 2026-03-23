7977b1b2d60606c684e2d0c02acf0027e7afcf22
Madhavi <9113237@ulta.com>
2026-03-23 14:13:15 +0530

    test

--- call-center-return/packages/return-search/src-custom/app/app-customization.impl.ts
+++ call-center-return/packages/return-search/src-custom/app/app-customization.impl.ts
@@ -1,10 +1,7 @@
-// TEST CASE 1: Missing import - SampleSharedComponent should be imported from @call-center/return-shared
-// TEST CASE 2: Missing import - SummaryNotesPanelComponent should be imported from @call-center/return-shared
-// TEST CASE 3: Wrong import - ItemImageComponent has wrong path
+
 import { ItemImageComponent } from './wrong/path/item-image.component';
 export class AppCustomizationImpl {
     static readonly components = [
-        SampleSharedComponent,
         SummaryNotesPanelComponent,
         ItemImageComponent
     ];
